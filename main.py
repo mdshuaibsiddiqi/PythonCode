b4df8afca4f9a094ea7ad24cd88a2851fb637619
Mohammad Shuab Siddique <shuaib.siddiqi@gmail.com>
2021-12-15 11:28:44 -0700

    Uploads of sample program during training

---
+++ main.py
@@ -0,0 +1,14 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+acronyms = {}
+acronyms['lol'] = 'Laugh Out Loud'
+acronyms['IDK'] = 'I Dont know'
+acronyms['IDK'] = 'To Be Honest'
+
+acro = {'wth':'What The Hell',
+        'wfh': 'Work From Home',
+        'brb': 'Be Right Back'}
+
+print(acronyms['lol'])
+print(acro['brb'])
